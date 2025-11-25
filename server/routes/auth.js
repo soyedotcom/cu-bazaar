3d2d834b695f6e75c546cd2516b99fe6162d3b67
Soye Usoroh <usorohsoye@gmail.com>
2025-11-25 13:19:57 +0100

    create authentication route template

---
+++ server/routes/auth.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import User from "../models/userModel.js";
+
+const router = express.Router();
+
+router.post("/sign-up", async (req, res) => {
+  try {
+  } catch (err) {}
+});
+
+router.post("/sign-in", async (req, res) => {
+  try {
+  } catch (err) {}
+});
--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+
+import authRouter from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(cors());
+app.use("/cu-bazaar/auth", authRouter);
 
 const connectToDB = () => {
   mongoose.connect(MONGODB_URL).then(() => {
@@ -14,6 +20,6 @@ const connectToDB = () => {
 };
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
   connectToDB();
+  console.log(`App running on port ${PORT}`);
 });
