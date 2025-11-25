20b835800a7f8ef6bc93c9de1b500aeb0a5c3667
Soye Usoroh <usorohsoye@gmail.com>
2025-11-25 09:52:32 +0100

    add local db connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
 
+const connectToDB = () => {
+  mongoose.connect(MONGODB_URL).then(() => {
+    console.log("connected to database");
+  });
+};
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
+  connectToDB();
 });
