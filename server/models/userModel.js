39228902eba89eb16e4835e4ce94b11b3a3df1ee
Soye Usoroh <usorohsoye@gmail.com>
2025-11-25 12:49:24 +0100

    update user schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,23 +4,35 @@ const userSchema = mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, "Please enter your first name"],
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, "Please enter your last name"],
     },
     matric: {
       type: String,
-      required: false,
     },
     regno: {
+      type: Number,
+      required: [true, "Please enter your reg number"],
+    },
+    email: {
       type: String,
+      required: [true, "Please enter your student email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter password"],
+    },
+    isSeller: {
+      type: Bool,
       required: true,
+      default: false,
     },
   },
   { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
-export default Book;
+export default User;
