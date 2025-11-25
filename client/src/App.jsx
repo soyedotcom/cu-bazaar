deaff4964e6d9d0b42fa130681a39de2a417c866
Soye Usoroh <usorohsoye@gmail.com>
2025-11-25 13:35:45 +0100

    add routes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/SignUp";
+import UserProfile from "./pages/SignUp";
+import SellerProfile from "./pages/SignUp";
+import ProductDetails from "./pages/SignUp";
+import UserCart from "./pages/SignUp";
+
 const App = () => {
-  return <div>Welcome to App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/sign-in" element={<SignUp />}></Route>
+        <Route path="/:id/profile" element={<UserProfile />}></Route>
+        <Route path="/:id/shop" element={<SellerProfile />}></Route>
+        <Route path="/:id/details" element={<ProductDetails />}></Route>
+        <Route path="/:id/cart" element={<UserCart />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/pages/UsersProfile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UsersProfile = () => {
-  return (
-    <div>UsersProfile</div>
-  )
-}
-
-export default UsersProfile
