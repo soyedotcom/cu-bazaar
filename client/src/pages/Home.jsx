056f5582b690ffaba49d25501fe1f157fd37f7cb
Soye Usoroh <usorohsoye@gmail.com>
2025-11-25 13:59:40 +0100

    create template links in home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>Welcome Back User!</h2>
+
+      <div>
+        <p>
+          Create an account : <Link to="/sign-up">Sign Up</Link>
+        </p>
+        <p>
+          Log back in : <Link to="/sign-in">Sign In</Link>
+        </p>
+        <p>
+          Go to user profile : <Link to="/:id/profile">User</Link>
+        </p>
+        <p>
+          Go to seller profile : <Link to="/:id/shop">Shop</Link>
+        </p>
+        <p>
+          Go to product details : <Link to="/:id/details">Product</Link>
+        </p>
+        <p>
+          Go to cart : <Link to="/:id/cart">Cart</Link>
+        </p>
+        <p>
+          Back to home : <Link to="/">Home</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
