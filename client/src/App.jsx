f38b4ac674a05ee2a86cf7e577abae03a86d65f6
Soye Usoroh <usorohsoye@gmail.com>
2026-04-01 21:10:08 +0100

    finish navbar for desktop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1>cu bazzar</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,43 @@
+import DeliveryIcon from "@mui/icons-material/MopedOutlined";
+import ProfileIcon from "@mui/icons-material/PersonOutlineRounded";
+import WishlistIcon from "@mui/icons-material/FavoriteBorderRounded";
+import CartIcon from "@mui/icons-material/ShoppingBagOutlined";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      role="navigation"
+      aria-label="navigation bar"
+      class="bg-blue-300 flex flex-nowrap flex-auto justify-between align-bottom font-bold ml-[100px] mr-[100px]"
+    >
+      {/* logo section */}
+      <div class="text-[32px]">cubazzar</div>
 
-export default Navbar
+      {/* page links */}
+      <div class="flex justify-between gap-[48px]">
+        <div>Shop</div>
+        <div>Services</div>
+        <div>Deals & Discounts</div>
+        <div>News & Events</div>
+      </div>
+
+      {/* user interaction icons*/}
+      <div class="flex justify-between gap-[21px]">
+        <div>
+          <DeliveryIcon />
+        </div>
+        <div>
+          <ProfileIcon />
+        </div>
+        <div>
+          <WishlistIcon />
+        </div>
+        <div>
+          <CartIcon />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
