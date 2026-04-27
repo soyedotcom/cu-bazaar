633c29d7409ff9132b04e8b1e01cc29dfa5d9013
Soye Usoroh <usorohsoye@gmail.com>
2026-04-27 14:28:40 +0100

    create productcard and addtocartcard with boiler code

---
+++ client/src/components/AddToCartCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddToCartCard = () => {
+  return (
+    <main className="bg-gray-950">
+      <section className="bg-white"></section>
+    </main>
+  );
+};
+
+export default AddToCartCard;
--- client/src/pages/Showcase.jsx
+++ client/src/pages/Showcase.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Navbar from "../components/Navbar";
+import ProductCard from "../components/ProductCard";
 
 // component showcase
 const Showcase = () => {
   return (
     <main>
-      <Navbar />
+      {/* <Navbar /> */}
+      <ProductCard />
     </main>
   );
 };
