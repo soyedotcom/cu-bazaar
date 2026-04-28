b17a929f66a4d9d9825320617687584b4dee2590
Soye Usoroh <usorohsoye@gmail.com>
2026-04-28 16:14:13 +0100

    add display styles

--- client/src/components/AddToCartCard.tsx
+++ client/src/components/AddToCartCard.tsx
@@ -5,27 +5,33 @@ import LinkIcon from "@mui/icons-material/ArrowForward";
 
 const AddToCartCard = () => {
   return (
-    <main className="bg-[#00000017] w-screen h-screen z-50">
+    <main className="bg-[#00000017] w-[100vh - 5] h-screen z-50">
       <section className="bg-green-200 flex flex-row w-200 h-100 rounded-xl p-4">
         <section className="bg-blue-300 flex-1 rounded-l-xl">
-          <img src="" alt="product image" />
+          <img
+            className="rounded-xl h-full w-full"
+            src=""
+            alt="product image"
+          />
         </section>
 
-        <section className="bg-pink-300 flex-1 rounded-r-xl">
-          <section>
-            <p>Product Name</p>
-            <p>Product Vendor</p>
+        <section className="bg-pink-300 flex-1 rounded-r-xl text-left pl-5">
+          <section className="mb-5">
+            <p className="font-bold">Product Name</p>
+            <p>
+              Sold by <span className="font-bold">Product Vendor</span>
+            </p>
           </section>
 
           <section>
-            <p>Product Price</p>
+            <p className="font-bold">Product Price</p>
             <img src="" alt="Product Rating" />
           </section>
 
-          <div>Procuct Specifications</div>
+          <div className="flex-1">Procuct Specifications</div>
 
           <section>
-            <div className="flex">
+            <div className="flex items-center">
               <div>
                 <Link to="/shop">
                   <button className="bg-purple-400 border-none rounded-full h-12 w-60.75 hover:cursor-pointer">
