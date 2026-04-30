aa235b8f2b6ac4890180ba7ccf540b3057648864
Soye Usoroh <usorohsoye@gmail.com>
2026-04-30 12:32:00 +0100

    add productcard to showcase

--- client/src/pages/Showcase.tsx
+++ client/src/pages/Showcase.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-
-import Navbar from "../components/Navbar";
 import ProductCard from "../components/ProductCard";
-import AddToCartCard from "../components/AddToCartCard";
 
 // component showcase
 const Showcase = () => {
   return (
     <main className="flex flex-col gap-4">
-      {/* <Navbar /> */}
-      {/* <ProductCard /> */}
-      <AddToCartCard />
+      <ProductCard />
     </main>
   );
 };
