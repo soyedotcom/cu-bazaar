956b7ab9f66441ea93f9a49cb970e8034c4c5272
Soye Usoroh <usorohsoye@gmail.com>
2026-05-05 13:49:16 +0100

    create wishlistproductcard.tsx

--- client/src/components/SubNav.tsx
+++ client/src/components/SubNav.tsx
@@ -1,14 +1,36 @@
 import DropDownIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import { subNavData } from "./subNavData";
+import { useState } from "react";
 
 const SubNav = () => {
+  const [activeMenu, setActiveMenu] = useState<string>("Products");
+
   return (
-    <nav>
-      <div>
-        Products <DropDownIcon />
+    <nav className="flex flex-col gap-5 flex-auto justify-between align-bottom font-bold ml-25 mr-25 mt-5 mb-5">
+      <div className="flex flex-row gap-5">
+        {subNavData.map((category) => (
+          <div
+            className="hover:cursor-pointer"
+            key={category.name}
+            onMouseEnter={() => setActiveMenu(category.name)}
+          >
+            {category.name}
+            <span>
+              <DropDownIcon />
+            </span>
+          </div>
+        ))}
       </div>
 
-      <div>
-        
+      <div className="flex flex-row gap-4">
+        {subNavData
+          .find((category) => category.name === activeMenu)
+          ?.subcategories?.map((subcat) => (
+            <div key={subcat.name} className="sub-item">
+              <span className="icon">{subcat.icon}</span>
+              <span>{subcat.name}</span>
+            </div>
+          ))}
       </div>
     </nav>
   );
