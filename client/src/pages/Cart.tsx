03d617ecb2085927d8cbcac23773028caf73dd8d
Soye Usoroh <usorohsoye@gmail.com>
2026-05-07 10:22:03 +0100

    create cart page

---
+++ client/src/pages/Cart.tsx
@@ -0,0 +1,23 @@
+import OrderSummary from "../components/OrderSummary";
+import CartProductCard from "../components/CartProductCard";
+
+const Cart = () => {
+  return (
+    <main className="flex flex-col m-25">
+      <div className="flex flex-row">
+        <section className="flex-1 flex flex-col gap-10">
+          <h1 className="text-left text-[24px] font-bold">Cart (4)</h1>
+          <CartProductCard />
+          <CartProductCard />
+          <CartProductCard />
+          <CartProductCard />
+        </section>
+        <section>
+          <OrderSummary />
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Cart;
