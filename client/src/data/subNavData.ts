70fbfe855a1882829883d541455b4637756fec90
Soye Usoroh <usorohsoye@gmail.com>
2026-05-07 11:40:16 +0100

    add Section interface

--- client/src/data/subNavData.ts
+++ client/src/data/subNavData.ts
@@ -1,6 +1,11 @@
+export interface Section {
+  name: string;
+}
+
 export interface SubCategory {
   name: string;
   icon?: string;
+  sections?: Section[];
 }
 
 export interface Category {
@@ -11,15 +16,45 @@ export interface Category {
 export const subNavData: Category[] = [
   {
     name: "Products",
-    subcategories: [{ name: "Fashion" }, { name: "Food and Provisions" }],
+
+    subcategories: [
+      {
+        name: "Fashion",
+
+        sections: [
+          { name: "Tops" },
+          { name: "Bottoms" },
+          { name: "Shoes" },
+          { name: "Jewelry and Accessories" },
+        ],
+      },
+
+      {
+        name: "Food and Provisions",
+
+        sections: [{ name: "Snacks" }, { name: "Drinks" }],
+      },
+    ],
   },
+
   {
     name: "Services",
-    subcategories: [{ name: "Laundry" }, { name: "Hair Styling" }],
+
+    subcategories: [
+      {
+        name: "Laundry",
+      },
+
+      {
+        name: "Hair Styling",
+      },
+    ],
   },
+
   {
     name: "Deals and Discounts",
   },
+
   {
     name: "News and Events",
   },
