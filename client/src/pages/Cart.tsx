634ec72ccf381b0b733adcaae353f07da2d17de5
Soye Usoroh <usorohsoye@gmail.com>
2026-05-07 10:44:35 +0100

    adjust margin

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -3,10 +3,11 @@ import CartProductCard from "../components/CartProductCard";
 
 const Cart = () => {
   return (
-    <main className="flex flex-col m-25">
+    <main className="flex flex-col mx-25 my-15">
       <div className="flex flex-row">
         <section className="flex-1 flex flex-col gap-10">
-          <h1 className="text-left text-[24px] font-bold">Cart (4)</h1>
+          <h1 className="text-left text-[24px] font-bold">Cart (5)</h1>
+          <CartProductCard />
           <CartProductCard />
           <CartProductCard />
           <CartProductCard />
