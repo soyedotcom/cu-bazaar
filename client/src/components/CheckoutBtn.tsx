4ebc1204b017d7a073382d4e9d6b180a81780387
Soye Usoroh <usorohsoye@gmail.com>
2026-05-07 10:45:05 +0100

    adjust width

--- client/src/components/CheckoutBtn.tsx
+++ client/src/components/CheckoutBtn.tsx
@@ -3,7 +3,7 @@ const CheckoutBtn = () => {
   return (
     <>
       <Link to="/">
-        <button className="bg-purple-400 border-none rounded-full h-10.5 w-52 font-bold hover:cursor-pointer">
+        <button className="bg-purple-400 border-none rounded-full h-10 w-55 font-bold hover:cursor-pointer">
           <p>Checkout</p>
         </button>
       </Link>
