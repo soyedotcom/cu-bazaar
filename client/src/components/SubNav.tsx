f353a73859f06d806d14071e2873f44155f5a6ff
Soye Usoroh <usorohsoye@gmail.com>
2026-05-07 12:04:45 +0100

    finish subnav

--- client/src/components/SubNav.tsx
+++ client/src/components/SubNav.tsx
@@ -4,33 +4,55 @@ import { useState } from "react";
 
 const SubNav = () => {
   const [activeMenu, setActiveMenu] = useState<string>("Products");
+  const [activeSubCategory, setActiveSubCategory] = useState<string>("Fashion");
+
+  const currentCategory = subNavData.find(
+    (category) => category.name === activeMenu,
+  );
+
+  const currentSubcategory = currentCategory?.subcategories?.find(
+    (subcat) => subcat.name === activeSubCategory,
+  );
 
   return (
-    <nav className="flex flex-col gap-5 flex-auto justify-between align-bottom font-bold ml-25 mr-25 mt-5 mb-5">
-      <div className="flex flex-row gap-5">
+    <nav className="flex flex-col gap-6 ml-25 mr-25 mt-5 mb-5">
+      <div className="flex gap-8 font-bold">
         {subNavData.map((category) => (
           <div
-            className="hover:cursor-pointer"
             key={category.name}
             onMouseEnter={() => setActiveMenu(category.name)}
+            className={`flex gap-1 cursor-pointer pb-2 ${activeMenu === category.name ? "border-b-3 border-black " : "text-gray-500"}`}
           >
-            {category.name}
-            <span>
-              <DropDownIcon />
-            </span>
+            <span>{category.name}</span>
+            <DropDownIcon />
           </div>
         ))}
       </div>
 
-      <div className="flex flex-row gap-4">
-        {subNavData
-          .find((category) => category.name === activeMenu)
-          ?.subcategories?.map((subcat) => (
-            <div key={subcat.name} className="hover:cursor-pointer">
-              <span className="icon">{subcat.icon}</span>
-              <span>{subcat.name}</span>
-            </div>
+      <div className="flex gap-6">
+        {currentCategory?.subcategories?.map((subcat) => (
+          <button
+            key={subcat.name}
+            onClick={() => setActiveSubCategory(subcat.name)}
+            className={`cursor-pointer font-bold ${activeSubCategory === subcat.name ? "text-black" : "text-gray-500"}`}
+          >
+            {subcat.name}
+          </button>
+        ))}
+      </div>
+
+      <div className="flex flex-col text-left gap-5 my-10">
+        <div className="text-[32px] font-bold">{activeSubCategory}</div>
+        <div className="flex gap-5 text-sm">
+          {currentSubcategory?.sections?.map((section) => (
+            <button
+              key={section.name}
+              className="cursor-pointer hover:text-black text-gray-600"
+            >
+              {section.name}
+            </button>
           ))}
+        </div>
       </div>
     </nav>
   );
