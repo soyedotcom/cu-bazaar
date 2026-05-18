593dc1fc667707373cf60cf573af0a7685d37b87
Soye Usoroh <usorohsoye@gmail.com>
2026-05-18 12:31:08 +0100

    create signin form

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -7,80 +7,99 @@ const SignInForm = () => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    axios
-      .post("http://localhost:3030/login", { email, password })
-      .then((result) => {
-        console.log(result);
-        if (result.data === "Success") {
-          navigate("/home");
-        } else {
-          alert(`Error: ${result.data}`);
-        }
-      })
-      .catch((err) => console.log(err));
+  const handleSubmit = () => {
+    console.log("Hello");
   };
 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   axios
+  //     .post("http://localhost:3030/login", { email, password })
+  //     .then((result) => {
+  //       console.log(result);
+  //       if (result.data === "Success") {
+  //         navigate("/home");
+  //       } else {
+  //         alert(`Error: ${result.data}`);
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
   return (
-    <div className="flex flex-col items-center p-5 justify-center h-screen">
+    <div className="flex flex-col items-center p-5 justify-center gap-5">
       <form
         action="/signup"
         method="POST"
         onSubmit={handleSubmit}
-        className="flex flex-col bg-white p-5 rounded-xl w-100 m-4"
+        className="flex flex-col p-5 w-100"
       >
-        <h2 className="text-3xl font-semibold mb-2">Welcome back!</h2>
+        <h1 className="text-4xl font-bold mb-5">Welcome back!</h1>
 
-        <label htmlFor="email" className="font-semibold my-2">
-          Email:
-        </label>
+        <div className="flex flex-col gap-7 my-5">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="font-bold py-2.5 pl-2 text-left">
+              Email:
+            </label>
 
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter email"
-          required
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          className="border border-gray-400 rounded-xs px-4 py-2"
-        />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter email"
+              required
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="border border-gray-400 rounded-full px-4 py-2 h-12"
+            />
+          </div>
 
-        <label htmlFor="password" className="font-semibold my-2">
-          Password:
-        </label>
+          <div className="flex flex-col">
+            <label
+              htmlFor="password"
+              className="font-semibold my-2 text-left pl-2"
+            >
+              Password:
+            </label>
 
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Enter password"
-          required
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          className="border border-gray-400 rounded-xs px-4 py-2"
-        />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="border border-gray-400 rounded-full px-4 py-2 h-12"
+            />
+            <Link
+              to="/"
+              className="text-left text-[14px] pl-2 mt-2 cursor-pointer hover:text-purple-600"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+        </div>
 
         <button
           type="submit"
-          className="font-semibold my-4 bg-purple-400 text-white p-2 rounded-s cursor-pointer"
+          className="font-bold my-4 bg-purple-400 text-white p-2 h-12 w-full rounded-full cursor-pointer"
         >
-          Log In
+          Continue Shopping
         </button>
       </form>
 
-      <p>Don't have an account?</p>
-
-      <Link
-        to="/signup"
-        className="font-semibold my-2 border-gray-500 border p-2 rounded-xs text-center cursor-pointer"
-      >
-        Sign Up
-      </Link>
+      <p>
+        Don't have an account yet?{" "}
+        <span>
+          <Link to="/signup" className="cursor-pointer hover:text-purple-600">
+            Sign Up
+          </Link>
+        </span>
+      </p>
     </div>
   );
 };
