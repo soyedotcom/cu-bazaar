a4702e8b8c3619fe1ffcfd0300a00d1f352467dc
Soye Usoroh <usorohsoye@gmail.com>
2026-05-19 19:03:45 +0100

    add search feature

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,16 +1,36 @@
 import SearchIcon from "@mui/icons-material/SearchOutlined";
 
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 const SearchBar = () => {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!query.trim()) return;
+    navigate(`/shop?q=${encodeURIComponent(query)}`);
+  };
+
   return (
     <>
-      <div className="flex items-center p-2.5 gap-3.75 border-2 rounded-full h-10.5 w-169">
+      <form
+        className="flex items-center p-2.5 gap-3.75 border-2 rounded-full h-10.5 w-169"
+        onSubmit={handleSearch}
+      >
         <SearchIcon />
         <input
           className="focus:outline-none"
           type="text"
           placeholder="Browse and buy items"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
         />
-      </div>
+      </form>
     </>
   );
 };
--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,12 +1,20 @@
 import { products } from "../data/products";
+import { useSearchParams } from "react-router-dom";
 import ProductDisplay from "../components/ProductDisplay";
 import SubNav from "../components/SubNav";
 
 const Shop = () => {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q")?.toLocaleLowerCase() || "";
+
+  const filteredProducts = products.filter((p) =>
+    p.name.toLocaleLowerCase().includes(query),
+  );
+
   return (
     <main className="flex flex-col mx-25 my-10">
       <SubNav />
-      <ProductDisplay products={products} />
+      <ProductDisplay products={filteredProducts} />
     </main>
   );
 };
