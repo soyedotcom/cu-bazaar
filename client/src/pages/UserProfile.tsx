d546763885a1169199dc6547c2161b914af30aeb
Soye Usoroh <usorohsoye@gmail.com>
2026-05-19 18:47:24 +0100

    adjust margin

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,5 +1,5 @@
 const UserProfile = () => {
-  return <main className="flex flex-col mx-25 my-15">User Profile</main>;
+  return <main className="flex flex-col mx-25 my-10">User Profile</main>;
 };
 
 export default UserProfile;
