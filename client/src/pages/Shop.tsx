016bd4db404e04f853c46cf71e443e74e08ed601
Soye Usoroh <usorohsoye@gmail.com>
2026-05-19 03:56:16 +0100

    import products to shop

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,9 +1,12 @@
-import SubNav from "../components/SubNav"
+import { products } from "../data/products";
+import ProductDisplay from "../components/ProductDisplay";
+import SubNav from "../components/SubNav";
 
 const Shop = () => {
   return (
     <div>
       <SubNav />
+      <ProductDisplay products={products} />
     </div>
   );
 };
