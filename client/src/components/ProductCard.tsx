20ee9c3539223abbf2e4fede7098142002656152
Soye Usoroh <usorohsoye@gmail.com>
2026-05-19 03:38:40 +0100

    replace hardcoded values with props

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,20 +1,27 @@
+import type { Product } from "../data/products";
+
 import WishlistIcon from "@mui/icons-material/FavoriteBorderRounded";
 import CartIcon from "@mui/icons-material/ShoppingBagOutlined";
 import InfoIcon from "@mui/icons-material/InfoOutlineRounded";
 
-const ProductCard = () => {
+interface Props {
+  product: Product;
+}
+
+const ProductCard = ({ product }: Props) => {
   return (
     <div className=" h-74 w-41 flex flex-col">
       <section className="w-full h-37">
         <img
           className="h-full w-full object-fill object-center"
-          src="shoe.jpg"
-          alt="product image"
+          src={product.image}
+          alt={product.name}
         />
       </section>
+
       <section className=" w-full h-37 flex flex-col text-left mt-4">
-        <div className="mb-1">Sport Shoes</div>
-        <div className="font-bold mb-5">N20,000.00</div>
+        <div className="mb-1">{product.name}</div>
+        <div className="font-bold mb-5">₦{product.price.toLocaleString()}</div>
         <div className="flex gap-2 align-middle">
           <button>
             <InfoIcon />
