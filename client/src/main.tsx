915b825e1ffe45c82d72b003c751d90a19eea041
Soye Usoroh <usorohsoye@gmail.com>
2026-05-19 21:02:36 +0100

    add cartprovider to main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
 // import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </StrictMode>,
 );
