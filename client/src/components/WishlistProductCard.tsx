75a9a8a1a3bc4ba8cf934d633f813e7055b16688
Soye Usoroh <usorohsoye@gmail.com>
2026-05-19 18:45:39 +0100

    update variables

--- client/src/components/WishlistProductCard.tsx
+++ client/src/components/WishlistProductCard.tsx
@@ -2,26 +2,41 @@ import AddToCartBtn from "./AddToCartBtn";
 import WishlistIcon from "@mui/icons-material/FavoriteBorderRounded";
 import InfoIcon from "@mui/icons-material/InfoOutlineRounded";
 
-const WishlistProductCard = () => {
+import { Link } from "react-router-dom";
+import type { WishListItem } from "../data/wishlist";
+
+interface Props {
+  wishListItem: WishListItem;
+}
+
+const WishlistProductCard = ({ wishListItem }: Props) => {
+  const { product } = wishListItem;
+
   return (
     <section className="flex flex-row h-39 w-120">
       <div>
         <img
           className="h-full w-39 object-center object-cover"
-          src="glasses.jpg"
-          alt="product image"
+          src={product.image}
+          alt={product.name}
         />
       </div>
       <div className="flex-1 flex flex-col text-left ml-5">
         <div className="flex justify-between mb-1">
-          <div>Product Name</div>
+          <div>{product.name}</div>
           <div className="flex gap-2">
-            <InfoIcon className="hover:cursor-pointer" />
+            <Link
+              to={`/product/${product.id}`}
+              className="hover:cursor-pointer"
+            >
+              <InfoIcon />
+            </Link>
+
             <WishlistIcon className="hover:cursor-pointer" />
           </div>
         </div>
         <div>
-          <p className="font-bold">Product Price</p>
+          <p className="font-bold">₦{product.price.toLocaleString()}</p>
         </div>
         <div className="mt-auto text-right">
           <AddToCartBtn />
--- client/src/pages/Wishlist.tsx
+++ client/src/pages/Wishlist.tsx
@@ -1,13 +1,22 @@
 import WishlistProductCard from "../components/WishlistProductCard";
+import { wishlist } from "../data/wishlist";
 
 const Wishlist = () => {
   return (
     <main className="flex flex-col mx-25 my-15">
       <section className="flex-1 flex flex-col gap-10">
-        <h1 className="text-left text-[24px] font-bold">Wishlist (3)</h1>
-        <WishlistProductCard />
-        <WishlistProductCard />
-        <WishlistProductCard />
+        <h1 className="text-left text-[24px] font-bold">
+          Wishlist ({wishlist.length})
+        </h1>
+
+        {wishlist.map((wishlistItem) => {
+          return (
+            <WishlistProductCard
+              key={wishlistItem.product.id}
+              wishListItem={wishlistItem}
+            />
+          );
+        })}
       </section>
     </main>
   );
