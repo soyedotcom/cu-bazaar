94151d86aca26ef8dd5df54ebe5e567fca49cb7d
Soye Usoroh <usorohsoye@gmail.com>
2026-05-21 22:51:12 +0100

    add template code to product route

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -6,4 +6,16 @@ router.get("/", (req, res) => {
   res.send("Products endpoint is working!");
 });
 
+router.post("/add", (req, res) => {
+  res.send("Add Product endpoint working!");
+});
+
+router.put("/update", (req, res) => {
+  res.send("Update Product endpoint working!");
+});
+
+router.delete("/delete", (req, res) => {
+  res.send("Delete Product endpoint working!");
+});
+
 export default router;
