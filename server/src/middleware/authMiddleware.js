fe00f72e6ee864cfb3f2a0a189124faf17a2cf08
Soye Usoroh <usorohsoye@gmail.com>
2026-05-22 10:01:17 +0100

    create authMiddleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../config/database.js";
+
+export const authMiddleware = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies?.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await prisma.user.findUnique({ where: { id: decoded.id } });
+
+    if (!user) {
+      return res.status(401).json({ error: "User doesn't exist." });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: "Token failed." });
+  }
+};
