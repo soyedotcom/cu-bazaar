0a18f0b5ac3b4ad56a517177ab49c651604941bc
Soye Usoroh <usorohsoye@gmail.com>
2026-05-24 12:49:58 +0100

    add unable to find products tag

--- client/src/components/ProductDisplay.tsx
+++ client/src/components/ProductDisplay.tsx
@@ -6,6 +6,13 @@ interface Props {
 }
 
 const ProductDisplay = ({ products }: Props) => {
+  if (products.length === 0) {
+    return (
+      <p>
+        Unable to find what you're looking for. Try using different key words
+      </p>
+    );
+  }
   return (
     <section className="grid grid-cols-6 justify-between py-10 gap-y-5">
       {products.map((product) => (
