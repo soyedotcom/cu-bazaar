d593b2c877ffbbfbf5482eadf762f749ae6eeb2e
Soye Usoroh <usorohsoye@gmail.com>
2026-05-28 12:14:10 +0100

    add gif to allowed formats

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -6,7 +6,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "cu-bazaar",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    allowed_formats: ["jpg", "jpeg", "png", "webp", "gif"],
   },
 });
 
