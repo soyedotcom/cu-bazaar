0e0805fb7367d4d836563bdb8db6de3bb4b12c47
Soye Usoroh <usorohsoye@gmail.com>
2026-06-10 12:33:45 +0100

    allow cross-site token

--- server/src/utils/createToken.js
+++ server/src/utils/createToken.js
@@ -9,8 +9,8 @@ export const createToken = (userId, userRole, res) => {
 
   res.cookie("jwt_cookie", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000 * 7,
   });
   return token;
