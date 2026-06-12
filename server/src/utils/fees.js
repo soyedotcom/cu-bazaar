ffbf45dbd276c68ee903418eb8164eb23c68704c
Soye Usoroh <usorohsoye@gmail.com>
2026-06-12 16:18:19 +0100

    update fee payment

--- server/src/utils/fees.js
+++ server/src/utils/fees.js
@@ -1,6 +1,6 @@
 export const calculateFees = (price, quantity) => {
   const subtotal = price * quantity;
-  const platformFeePercent = Number(process.env.PLATFORM_FEE_PERCENT) || 5;
+  const platformFeePercent = Number(process.env.PLATFORM_FEE_PERCENT);
   const platformFee = subtotal * (platformFeePercent / 100);
   const sellerAmount = subtotal - platformFee;
   return { subtotal, platformFee, sellerAmount };
